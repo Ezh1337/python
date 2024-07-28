9838473fd9fa65a4e5ab3e1b23132358d46ceac9
Ezh1337 <illiaq20@gmail.com>
2024-07-28 13:34:05 +0200

    pyrthon tryout

---
+++ data.py
@@ -0,0 +1,8 @@
+x = 7 #int
+digit = 5.3414 #float
+boolean = True #bool
+result = "result : " # string
+print (result, x)
+print (result, digit)
+print (result, boolean)
+
---
+++ start.py
@@ -0,0 +1,9 @@
+#my first py project woohoo
+print("Hello world!")
+print(52, "hi everyone", sep="", end="?")
+print("tihi")
+print(min(3,5,1,4))
+print(max(3,57,7,4,32,2))
+print(abs(-59))
+print(pow(52,2))
+print(round(5/3))
